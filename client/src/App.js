1bb2d264235f3bb77e28caaac618770d190aacb0
Erica Melo de Carvalho <ericamcar28@gmail.com>
2021-07-18 15:22:40 +0100

    refactor routes for reusability

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
-import { setAuthInfo } from './store/authSlice';
 import UserProfilePage from './pages/UserProfilePage';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
@@ -18,8 +17,24 @@ import OrdersAdminPage from './pages/admin/OrdersAdminPage';
 import ClientsAdminPage from './pages/admin/ClientsAdminPage';
 import NewProductPage from './pages/admin/NewProductPage';
 
-function App() {
+const AdminRoute = ({ children, ...rest }) => {
   const { isAuthenticated, userInfo } = useSelector((state) => state.auth);
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        isAuthenticated && userInfo.isAdmin ? (
+          <>{children}</>
+        ) : (
+          <Redirect to='/login' />
+        )
+      }
+    />
+  );
+};
+
+function App() {
+  const { isAuthenticated } = useSelector((state) => state.auth);
 
   return (
     <BrowserRouter>
@@ -70,52 +85,21 @@ function App() {
           }
         />
 
-        <Route
-          exact
-          path='/admin/produtos'
-          render={() =>
-            isAuthenticated && userInfo.isAdmin ? (
-              <ProductsAdminPage />
-            ) : (
-              <Redirect to='/login' />
-            )
-          }
-        />
+        <AdminRoute exact path='/admin/produtos'>
+          <ProductsAdminPage />
+        </AdminRoute>
 
-        <Route
-          path='/admin/produtos/novo'
-          render={() =>
-            isAuthenticated && userInfo.isAdmin ? (
-              <NewProductPage />
-            ) : (
-              <Redirect to='/login' />
-            )
-          }
-        />
+        <AdminRoute path='/admin/produtos/novo'>
+          <NewProductPage />
+        </AdminRoute>
 
-        <Route
-          exact
-          path='/admin/pedidos'
-          render={() =>
-            isAuthenticated && userInfo.isAdmin ? (
-              <OrdersAdminPage />
-            ) : (
-              <Redirect to='/login' />
-            )
-          }
-        />
+        <AdminRoute path='/admin/pedidos'>
+          <OrdersAdminPage />
+        </AdminRoute>
 
-        <Route
-          exact
-          path='/admin/clientes'
-          render={() =>
-            isAuthenticated && userInfo.isAdmin ? (
-              <ClientsAdminPage />
-            ) : (
-              <Redirect to='/login' />
-            )
-          }
-        />
+        <AdminRoute path='/admin/clientes'>
+          <ClientsAdminPage />
+        </AdminRoute>
       </Switch>
       <Footer />
     </BrowserRouter>
