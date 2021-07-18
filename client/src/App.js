fd88e650e2f747819d81d84f2aa97b458b5f773e
Erica Melo de Carvalho <ericamcar28@gmail.com>
2021-07-18 17:52:38 +0100

    add lazy loading for private routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
-import UserProfilePage from './pages/UserProfilePage';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import AboutPage from './pages/AboutPage';
 import CartPage from './pages/CartPage';
 import ProductsPage from './pages/ProductsPage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
-import ProductsAdminPage from './pages/admin/ProductsAdminPage';
-import OrdersAdminPage from './pages/admin/OrdersAdminPage';
-import ClientsAdminPage from './pages/admin/ClientsAdminPage';
-import NewProductPage from './pages/admin/NewProductPage';
+import Loader from './components/Loader';
+
+// Lazy imports
+const UserProfilePage = lazy(() => import('./pages/UserProfilePage'));
+const ProductsAdminPage = lazy(() => import('./pages/admin/ProductsAdminPage'));
+const OrdersAdminPage = lazy(() => import('./pages/admin/OrdersAdminPage'));
+const ClientsAdminPage = lazy(() => import('./pages/admin/ClientsAdminPage'));
+const NewProductPage = lazy(() => import('./pages/admin/NewProductPage'));
 
 const AdminRoute = ({ children, ...rest }) => {
   const { isAuthenticated, userInfo } = useSelector((state) => state.auth);
@@ -37,72 +40,80 @@ function App() {
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path='/'>
-          <HomePage />
-        </Route>
+    <Suspense
+      fallback={
+        <main className='container'>
+          <Loader />
+        </main>
+      }
+    >
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
 
-        <Route exact path='/produtos'>
-          <ProductsPage />
-        </Route>
+          <Route exact path='/produtos'>
+            <ProductsPage />
+          </Route>
 
-        <Route exact path='/produtos/:id'>
-          <ProductDetailsPage />
-        </Route>
+          <Route exact path='/produtos/:id'>
+            <ProductDetailsPage />
+          </Route>
 
-        <Route path='/sobre-nos'>
-          <AboutPage />
-        </Route>
+          <Route path='/sobre-nos'>
+            <AboutPage />
+          </Route>
 
-        <Route path='/contato'>
-          <ContactPage />
-        </Route>
+          <Route path='/contato'>
+            <ContactPage />
+          </Route>
 
-        <Route path='/sacola'>
-          <CartPage />
-        </Route>
+          <Route path='/sacola'>
+            <CartPage />
+          </Route>
 
-        <Route
-          path='/conta'
-          render={() =>
-            isAuthenticated ? <UserProfilePage /> : <Redirect to='/login' />
-          }
-        />
+          <Route
+            path='/conta'
+            render={() =>
+              isAuthenticated ? <UserProfilePage /> : <Redirect to='/login' />
+            }
+          />
 
-        <Route
-          path='/cadastro'
-          render={() =>
-            !isAuthenticated ? <SignupPage /> : <Redirect to='/produtos' />
-          }
-        />
+          <Route
+            path='/cadastro'
+            render={() =>
+              !isAuthenticated ? <SignupPage /> : <Redirect to='/produtos' />
+            }
+          />
 
-        <Route
-          path='/login'
-          render={() =>
-            !isAuthenticated ? <LoginPage /> : <Redirect to='/produtos' />
-          }
-        />
+          <Route
+            path='/login'
+            render={() =>
+              !isAuthenticated ? <LoginPage /> : <Redirect to='/produtos' />
+            }
+          />
 
-        <AdminRoute exact path='/admin/produtos'>
-          <ProductsAdminPage />
-        </AdminRoute>
+          <AdminRoute exact path='/admin/produtos'>
+            <ProductsAdminPage />
+          </AdminRoute>
 
-        <AdminRoute path='/admin/produtos/novo'>
-          <NewProductPage />
-        </AdminRoute>
+          <AdminRoute path='/admin/produtos/novo'>
+            <NewProductPage />
+          </AdminRoute>
 
-        <AdminRoute path='/admin/pedidos'>
-          <OrdersAdminPage />
-        </AdminRoute>
+          <AdminRoute path='/admin/pedidos'>
+            <OrdersAdminPage />
+          </AdminRoute>
 
-        <AdminRoute path='/admin/clientes'>
-          <ClientsAdminPage />
-        </AdminRoute>
-      </Switch>
-      <Footer />
-    </BrowserRouter>
+          <AdminRoute path='/admin/clientes'>
+            <ClientsAdminPage />
+          </AdminRoute>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
